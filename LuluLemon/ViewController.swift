1ee4623d5d24156a51b6efed238f04bbb7758e25
Ronald Jones <rondjonesjr@gmail.com>
2022-11-19 12:09:40 -0500

    Initial Commit

---
+++ LuluLemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuluLemon
+//
+//  Created by Ronald Jones on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LuluLemonUITests/LuluLemonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LuluLemonUITestsLaunchTests.swift
+//  LuluLemonUITests
+//
+//  Created by Ronald Jones on 11/19/22.
+//
+
+import XCTest
+
+class LuluLemonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
